e1062347340321424bbf32c6c8b1147b58caef8e
wiechciu <wiechciu@gmail.com>
2025-08-08 23:57:51 +0200

    Added small typo in the code comment

--- core/scripts/change_log.gd
+++ core/scripts/change_log.gd
@@ -10,17 +10,17 @@ func _ready() -> void:
 func markdown_to_bbcode(markdown_text: String) -> String:
 	var text: String = markdown_text
 	
-	# H1 headers: # Something -> [font_size=24][b][u]Something[/u][/b]
+	# H1 headers: # Something -> [font_size=24][b][u]Something[/u][/b][/font_size]
 	var h1_regex: RegEx = RegEx.new()
 	h1_regex.compile(r"(?m)^# (.+)$")
 	text = h1_regex.sub(text, "[font_size=24][b][u]$1[/u][/b][/font_size]", true)
 	
-	# H2 headers: ## Something -> [font_size=20][b][u]Something[/u][/b]
+	# H2 headers: ## Something -> [font_size=20][b][u]Something[/u][/b][/font_size]
 	var h2_regex: RegEx = RegEx.new()
 	h2_regex.compile(r"(?m)^## (.+)$")
 	text = h2_regex.sub(text, "[font_size=20][b][u]$1[/u][/b][/font_size]", true)
 	
-	# H3 headers: ### Something -> [font_size=16][b]Something[/b]
+	# H3 headers: ### Something -> [font_size=16][b]Something[/b][/font_size]
 	var h3_regex: RegEx = RegEx.new()
 	h3_regex.compile(r"(?m)^### (.+)$")
 	text = h3_regex.sub(text, "[font_size=16][b]$1[/b][/font_size]", true)
