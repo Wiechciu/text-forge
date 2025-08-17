d248884ace135db4dd5e05492048a5db765c0107
mkh-user <khalili1388mahan@gmail.com>
2025-08-17 19:36:38 +0330

    Refactor Mode Manager

--- action_scripts/scenes/mode_manager.gd
+++ action_scripts/scenes/mode_manager.gd
@@ -2,8 +2,8 @@ extends Window
 
 @export var mode_list: ItemList
 @export var about: VBoxContainer
-@export var package_button: Button
-var modes: PackedStringArray
+@export var mode_kit_button: Button
+var mode_informations: Array[Dictionary]
 var current_mode_index: int
 
 func _ready() -> void:
@@ -12,33 +12,29 @@ func _ready() -> void:
 	_load_mode_list()
 
 
-func _show_about(idx) -> void:
-	if mode_list.select_mode == ItemList.SELECT_MULTI: return
-	var config = ConfigFile.new()
-	current_mode_index = idx
-	config.load("user://modes".path_join(modes[idx]).path_join("mode.cfg"))
-	about.get_child(0).text = config.get_value("mode", "name", "")
-	about.get_child(1).text = str(config.get_value("mode", "version", 0))
-	about.get_child(2).text = config.get_value("mode", "author", "")
-	about.get_child(3).text = ", ".join(config.get_value("mode", "extensions", []))
-	about.get_child(4).text = config.get_value("mode", "description", "")
-	about.show()
-
-
 func _load_mode_list() -> void:
 	mode_list.clear()
-	modes = DirAccess.get_directories_at("user://modes")
-	var config = ConfigFile.new()
-	for item in modes:
-		config.load("user://modes".path_join(item).path_join("mode.cfg"))
-		mode_list.add_item(config.get_value("mode", "name", "Unnamed mode"))
+	mode_informations = Global.get_editor_api().mode_list
+	for item in mode_informations:
+		mode_list.add_item(item["name"])
+
+
+func _show_about(idx) -> void:
+	if mode_list.select_mode == ItemList.SELECT_MULTI:
+		return
+	current_mode_index = idx
+	about.get_child(0).text = mode_informations[idx]["name"]
+	about.get_child(1).text = mode_informations[idx]["version"]
+	about.get_child(2).text = mode_informations[idx]["author"]
+	about.get_child(3).text = ", ".join(mode_informations[idx]["extensions"])
+	about.get_child(4).text = mode_informations[idx]["description"]
+	about.show()
 
 
 func _on_import_pressed() -> void:
 	add_child(Factory.file_dialog(FileDialog.FILE_MODE_OPEN_FILE, FileDialog.ACCESS_FILESYSTEM, ["*.tfmode,*.zip;Text Forge Modes;application/zip"], _import_mode, true, OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS)))
 
 
-# from official docs
 func _import_mode(path: String) -> void:
 	var reader = ZIPReader.new()
 	var err := reader.open(path)
@@ -71,17 +67,17 @@ func _close() -> void:
 
 func _on_data_changed(new_text: String) -> void:
 	var config = ConfigFile.new()
-	config.load("user://modes".path_join(modes[current_mode_index]).path_join("mode.cfg"))
+	config.load("user://modes".path_join(mode_informations[current_mode_index]["id"]).path_join("mode.cfg"))
 	config.set_value("mode", "name", about.get_child(0).text.strip_edges())
 	config.set_value("mode", "version", about.get_child(1).text.strip_edges())
 	config.set_value("mode", "author", about.get_child(2).text.strip_edges())
 	config.set_value("mode", "extensions", Array(about.get_child(3).text.split(",")).map(func(item: String): return item.strip_edges()))
 	config.set_value("mode", "description", about.get_child(4).text.strip_edges())
-	config.save("user://modes".path_join(modes[current_mode_index]).path_join("mode.cfg"))
+	config.save("user://modes".path_join(mode_informations[current_mode_index]["id"]).path_join("mode.cfg"))
 
 
 func _on_edit_script_pressed() -> void:
-	Signals.open_file.emit(SLib.globalize_path("user://modes".path_join(modes[current_mode_index]).path_join("mode.gd")))
+	Signals.open_file.emit(SLib.globalize_path("user://modes".path_join(mode_informations[current_mode_index]["id"]).path_join("mode.gd")))
 
 
 func _on_export_pressed() -> void:
@@ -94,7 +90,7 @@ func _export_mode(path: String) -> void:
 	if err != OK:
 		Global.send_notification(Global.Notification.ERROR, "Cann't export mode!", "Error code: " + str(err))
 		return
-	_add_folder_to_zip(writer, "user://modes/".path_join(modes[current_mode_index]))
+	_add_folder_to_zip(writer, "user://modes/".path_join(mode_informations[current_mode_index]["id"]))
 	writer.close()
 	Global.send_notification(Global.Notification.INFO, "Export mode completed.", "Exported file: " + path)
 
@@ -117,7 +113,7 @@ func _on_remove_pressed() -> void:
 	confirm.popup_centered()
 
 func _remove_mode() -> void:
-	OS.move_to_trash(SLib.globalize_path("user://modes".path_join(modes[current_mode_index])))
+	OS.move_to_trash(SLib.globalize_path("user://modes".path_join(mode_informations[current_mode_index]["id"])))
 	Global.send_notification(Global.Notification.INFO, "Remove mode completed.")
 	Global.get_editor_api().reload_modes()
 	_load_mode_list()
@@ -131,21 +127,21 @@ func _save_package(path: String) -> void:
 		Global.send_notification(Global.Notification.ERROR, "Cann't export mode kit!", "Error code: " + str(err))
 		return
 	for index in mode_list.get_selected_items():
-		_add_folder_to_zip(writer, path.path_join(modes[index]))
+		_add_folder_to_zip(writer, path.path_join(mode_informations[index]["id"]))
 	writer.close()
 	Global.send_notification(Global.Notification.INFO, "Export mode kit completed.", "Exported file: " + path)
 	mode_list.select_mode = ItemList.SELECT_SINGLE
 	mode_list.deselect_all()
 
 
-func _on_create_package_toggled(toggled_on: bool) -> void:
+func _on_create_kit_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		package_button.text = "Export Mode Kit..."
+		mode_kit_button.text = "Export Mode Kit..."
 		mode_list.select_mode = ItemList.SELECT_MULTI
 		mode_list.deselect_all()
 		about.hide()
 	else:
-		package_button.text = "Create Mode Kit..."
+		mode_kit_button.text = "Create Mode Kit..."
 		if mode_list.get_selected_items() == PackedInt32Array():
 			mode_list.select_mode = ItemList.SELECT_SINGLE
 			mode_list.deselect_all()
