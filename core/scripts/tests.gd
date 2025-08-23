b8de104301dcebe600678eaf21cb1d703459262c
mkh-user <khalili1388mahan@gmail.com>
2025-08-23 13:57:56 +0330

    Add queue_free for tests

--- core/scripts/tests.gd
+++ core/scripts/tests.gd
@@ -5,8 +5,10 @@ const PERFORMANCE_TEST := true
 
 func _ready() -> void:
 	if not OS.has_feature("editor"):
+		queue_free()
 		return
 	if DISABLE_ALL:
+		queue_free()
 		return
 	if PERFORMANCE_TEST:
 		add_child(load("res://tests/performance.gd").new())
