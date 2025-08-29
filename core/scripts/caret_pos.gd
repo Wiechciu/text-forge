e65cb9dfc21add6021e587584f0179f7affd39d0
mkh-user <khalili1388mahan@gmail.com>
2025-08-29 11:31:54 +0330

    Add move to feature for caret pos

--- core/scripts/caret_pos.gd
+++ core/scripts/caret_pos.gd
@@ -1,4 +1,7 @@
-extends Label
+extends Button
+
+@export var line: LineEdit
+@export var column: LineEdit
 
 func _ready() -> void:
 	Global.get_editor().caret_changed.connect(_update_caret_pos)
@@ -6,3 +9,20 @@ func _ready() -> void:
 
 func _update_caret_pos() -> void:
 	text = "{0} : {1}".format([Global.get_editor().get_caret_line(), Global.get_editor().get_caret_column()])
+
+
+func _on_popup_panel_about_to_popup() -> void:
+	line.text = str(Global.get_editor().get_caret_line())
+	column.text = str(Global.get_editor().get_caret_column())
+
+
+func _on_go_pressed() -> void:
+	Global.get_editor().set_caret_line(int(line.text))
+	Global.get_editor().set_caret_column(int(column.text))
+	get_child(0).hide()
+	Global.get_editor().grab_focus()
+
+
+func _on_pressed() -> void:
+	get_child(0).popup()
+	line.grab_focus()
